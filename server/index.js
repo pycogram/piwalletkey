f06721e616ada15e7805c97f1e20f22468d011ea
Ifesinachi Daniel <149059456+pycogram@users.noreply.github.com>
2025-02-24 15:31:07 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config({path: './config.env'});
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: [process.env.LOCAL_ORIGIN_URL], 
+    origin: [process.env.ONLINE_ORIGIN_URL], 
     methods: ["POST", "GET"],
     credentials: true
 }));
